6495774fd85b402886b01649b8906047b3076500
DIR\ilja.volinskis <email>
2026-05-25 15:23:27 +0300

    Add database setup

---
+++ app/src/main/java/com/example/shoppingcartapp/ShoppingCartApplication.kt
@@ -0,0 +1,9 @@
+package com.example.shoppingcartapp
+
+import android.app.Application
+
+class ShoppingCartApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
