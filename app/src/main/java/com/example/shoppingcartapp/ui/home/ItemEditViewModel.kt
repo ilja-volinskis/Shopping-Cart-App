c9a2d2a90f9b2069d4306b6bdcbb8f55a2d85b23
DIR\ilja.volinskis <email>
2026-06-04 14:10:27 +0300

    Update item edit view model

--- app/src/main/java/com/example/shoppingcartapp/ui/home/ItemEditViewModel.kt
+++ app/src/main/java/com/example/shoppingcartapp/ui/home/ItemEditViewModel.kt
@@ -6,12 +6,20 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.shoppingcartapp.data.CartRepository
 import com.example.shoppingcartapp.data.Item
 import com.example.shoppingcartapp.data.ItemsRepository
+import com.example.shoppingcartapp.ui.navigation.MainItemDetailsDestination
 import com.example.shoppingcartapp.ui.navigation.MainItemEditDestination
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,24 +28,45 @@ class ItemEditViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val itemsRepository: ItemsRepository
 ) : ViewModel() {
+    private val itemId: Int = checkNotNull(savedStateHandle[MainItemDetailsDestination.itemIdArg])
 
-    var uiState by mutableStateOf(ItemEditUiState())
-        private set
-
-    private val itemId: Int = checkNotNull(savedStateHandle[MainItemEditDestination.itemIdArg])
-
-    // itemId < 0 if you want to create new item from this screen
-    init {
-        viewModelScope.launch {
-            uiState = if(itemId < 0) {
-                ItemEditUiState()
-            } else {
+    // if itemId < 0 we know we are creating a new item
+    val uiState: StateFlow<ItemEditUiState> =
+        (
+            if(itemId < 0)
+                flowOf(ItemEditUiState())
+             else
                 itemsRepository
                     .getItemStream(itemId)
                     .filterNotNull()
-                    .first()
-                    .toItemEditUiState(true)
-            }
+                    .map {
+                        ItemEditUiState(
+                            itemDetails = it.toItemDetails(),
+                            isEntryValid = false,
+                            isEditingEntry = true
+                        )
+                    }
+        ).stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = ItemEditUiState()
+        )
+
+    fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemsRepository.insertItem(item)
+        }
+    }
+
+    fun updateItem(item: Item) {
+        viewModelScope.launch {
+            itemsRepository.updateItem(item)
+        }
+    }
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch {
+            itemsRepository.deleteItem(item)
         }
     }
 
@@ -48,10 +77,12 @@ class ItemEditViewModel @Inject constructor(
 
 data class ItemEditUiState(
     val itemDetails: ItemDetails = ItemDetails(),
+    val isEditingEntry: Boolean = false,
     val isEntryValid: Boolean = false
 )
 
 fun Item.toItemEditUiState(isEntryValid: Boolean = false): ItemEditUiState = ItemEditUiState(
     itemDetails = this.toItemDetails(),
-    isEntryValid = isEntryValid
+    isEntryValid = isEntryValid,
+    isEditingEntry = true
 )
